55a05d9bf150421406ccde0728503574eb1478ad
david <davidbeglari@gamil.com>
2019-09-15 09:00:08 +0400

    Recipe details

--- src/app/Server/server.component.ts
+++ src/app/Server/server.component.ts
@@ -1,9 +1,38 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
 })
-export class ServerComponent {
-  name: 'Hello my dear';
+export class ServerComponent implements OnInit {
+  username: string;
+
+  serverStatus = 'online';
+
+  showSecret: boolean;
+
+  log = [];
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+
+    this.log.push(new Date());
+  }
+
+  moreThen() {
+    return this.log.length > 5;
+  }
+
+  getColor() {
+    return this.log.length > 5 ? 'red' : 'blue';
+  }
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+  onDoSomething() {
+    this.username = '';
+  }
 }
---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,29 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+
+import {Recipe} from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'Description', 'https://assets.epicurious.com/photos/5d49f1ea888da200097dce24/6:4/w_620%2Ch_413/TomatoesHerbs_RECIPE_073119_278.jpg'),
+    new Recipe('A recipe', 'Description', 'https://assets.epicurious.com/photos/5d49f1ea888da200097dce24/6:4/w_620%2Ch_413/TomatoesHerbs_RECIPE_073119_278.jpg'),
+    new Recipe('A test', 'Description', 'https://assets.epicurious.com/photos/5d49f1ea888da200097dce24/6:4/w_620%2Ch_413/TomatoesHerbs_RECIPE_073119_278.jpg'),
+    new Recipe('test recipe', 'Description', 'https://assets.epicurious.com/photos/5d49f1ea888da200097dce24/6:4/w_620%2Ch_413/TomatoesHerbs_RECIPE_073119_278.jpg'),
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
+
+}
