02e416601e2aa2d1b76f9adeffbd773120bddeaf
david <davidbeglari@gamil.com>
2019-09-16 15:13:24 +0400

    Make unless directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import {BasicHighlightDirective} from './basic-highlight/basic-highlight.directive';
+import {BetterHighlightDirective} from './highlight/better-highlight.directive';
+import { UnlessDirective } from './unless.directive';
 
 @NgModule({
   declarations: [
@@ -28,7 +30,9 @@ import {BasicHighlightDirective} from './basic-highlight/basic-highlight.directi
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    BasicHighlightDirective
+    BasicHighlightDirective,
+    BetterHighlightDirective,
+    UnlessDirective
   ],
   imports: [
     BrowserModule,
